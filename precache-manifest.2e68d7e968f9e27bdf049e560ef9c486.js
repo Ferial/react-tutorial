292df8750b3570f322a0646fdbe51f71fba4f1e3
Ferial <ferial@users.noreply.github.com>
2020-05-25 05:09:54 +0400

    Updates

---
+++ precache-manifest.2e68d7e968f9e27bdf049e560ef9c486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc0cb126690c18aef4df8cb224f550b",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "0b88caedfafa14262d65",
+    "url": "/react-tutorial/static/css/main.b044d04a.chunk.css"
+  },
+  {
+    "revision": "4762f349b9c98d1da948",
+    "url": "/react-tutorial/static/js/2.0bf4f24d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.0bf4f24d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b88caedfafa14262d65",
+    "url": "/react-tutorial/static/js/main.854f8fa9.chunk.js"
+  },
+  {
+    "revision": "b6f2badd4a096922496c",
+    "url": "/react-tutorial/static/js/runtime-main.ee9240c0.js"
+  }
+]);
